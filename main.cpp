7bf0bd522effd258eda0176832c922f98294d90a
Anton <mr.alligator@yandex.ru>
2020-02-03 00:48:20 +0300

    check list

--- main.cpp
+++ main.cpp
@@ -4,9 +4,21 @@ int main() {
     char _char = -1;
     short  _short = 0;
     int _int = 2130706433;
-    long long _LL = 8875824491850138409LL;
-    std::string str = "172.22.12.129";
+    long long _ll = 8875824491850138409LL;
+    std::string str = "45.67.89.101.112.131.41";
+    std::vector<int> v = {67,89,101,112,131,41};
+    std::list<int> l = {89,101,112,131,41};
+    std::tuple<int,int> t = {66,67};
+
+    /*print_ip(_char);
+    print_ip(_short);
+    print_ip(_int);
+    print_ip(_ll);
     print_ip(str);
+    print_ip(v);
+    print_ip(l);*/
+    print_ip(t);
+
 
     return 0;
 }
--- print_ip.h
+++ print_ip.h
@@ -2,9 +2,11 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <list>
 #include <iterator>
 #include <tuple>
 #include <type_traits>
+#include <tuple>
 
 using vector_uchar = std::vector<unsigned char>;
 
@@ -38,6 +40,9 @@ template<typename T>
 struct is_container<std::vector<T>> : public std::true_type {};
 
 template<typename T>
+struct is_container<std::list<T>> : public std::true_type {};
+
+template<typename T>
 typename std::enable_if<is_container<T>::value>::type
 print_ip (T &number)
 {
@@ -47,4 +52,19 @@ print_ip (T &number)
         ack.push_back(i);
     }
     print(ack);
+}
+
+template<typename T> struct is_tuple : public std::false_type {};
+
+template<typename ...Arg>
+struct is_tuple<std::tuple<Arg...>> : public std::true_type {};
+
+template<typename T>
+typename std::enable_if<is_tuple<T>::value>::type
+print_ip (T &number)
+{
+    for (auto i: number)
+    {
+        std::cout << i << std::endl;
+    }
 }
